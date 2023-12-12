83b23d77501e654b8cdd9eedefe37461f1644dda
Rex6z <cyrus6haywood@gmail.com>
2023-12-12 17:52:16 -0500

    fixed file names

---
+++ C/Data Structures Using C/C3E1.c
@@ -0,0 +1,16 @@
+/*
+Consider an array MARKS[20][5] which stores the 
+marks obtained by 20 students in 5 subjects. Now 
+write a program to
+(a) find the average marks obtained in each 
+subject.
+(b) find the average marks obtained by every 
+student.
+(c) find the number of students who have scored 
+below 50 in their average.
+(d) display the scores obtained by every student.
+*/
+
+void main(){
+    
+}
